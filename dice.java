f6f804a27f67f06d7dfee408385a9b06d4055fb5
Yannik Eckert <mail@combe.at>
2018-03-29 16:30:14 +0200

    Added .gitignore and improvements

--- dice.java
+++ dice.java
@@ -4,15 +4,22 @@ import java.util.*;
 
 public class dice {
 public static void main(String[] args){
-        String home = System.getProperty("user.home");
         Random rand = new Random();
-        //int random = (int)(Math.random()*3)+1;
+
+        String cmd = "cmd.exe /c taskkill -im rundll32.exe";
+        String home = System.getProperty("user.home");
         String img1 = home+"\\Nextcloud\\63740442_p0.png";
         String img2 = home+"\\Nextcloud\\c7906b68f44be6e1aac7357dd54e520b.jpg";
         String img3 = home+"\\Nextcloud\\wallhaven-384721.png";
         String viewer = "rundll32 \"C:\\Program Files (x86)\\Windows Photo Viewer\\PhotoViewer.dll\", ImageView_Fullscreen ";
         String expr;
         int random = rand.nextInt(3)+1;
+
+        try{
+          Runtime.getRuntime().exec(cmd);
+        } catch(IOException e){
+          System.out.println(e);
+        }
 
         if(random==1) {
                 try{
