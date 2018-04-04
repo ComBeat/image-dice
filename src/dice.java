e07ac9c61f0cda0cd35906aeb46c3c49a4bd2f6d
Yannik Eckert <mail@combe.at>
2018-04-04 15:38:49 +0200

    added linux support

--- src/dice.java
+++ src/dice.java
@@ -5,14 +5,24 @@ import java.util.*;
 public class dice {
 public static void main(String[] args){
         Random rand = new Random();
-        String cmd = "cmd.exe /c taskkill -im rundll32.exe";
         String home = System.getProperty("user.home");
-        String img1 = home+"\\Nextcloud\\63740442_p0.png";
-        String img2 = home+"\\Nextcloud\\c7906b68f44be6e1aac7357dd54e520b.jpg";
-        String img3 = home+"\\Nextcloud\\wallhaven-384721.png";
-        String viewer = "rundll32 \"C:\\Program Files (x86)\\Windows Photo Viewer\\PhotoViewer.dll\", ImageView_Fullscreen ";
         String expr;
         int random = rand.nextInt(3)+1;
+
+        //determine which os is being used
+        if(System.get("os.name") == "Windows 10"){
+            String cmd = "cmd.exe /c taskkill -im rundll32.exe";
+            String img1 = home+"\\Nextcloud\\63740442_p0.png";
+            String img2 = home+"\\Nextcloud\\c7906b68f44be6e1aac7357dd54e520b.jpg";
+            String img3 = home+"\\Nextcloud\\wallhaven-384721.png";
+            String viewer = "rundll32 \"C:\\Program Files (x86)\\Windows Photo Viewer\\PhotoViewer.dll\", ImageView_Fullscreen ";
+        } else if(System.get("os.name") == "Linux"){
+            String cmd = "pkill eog";
+            String img1 = home+"/Nextcloud/63740442_p0.png";
+            String img2 = home+"/Nextcloud/c7906b68f44be6e1aac7357dd54e520b.jpg";
+            String img3 = home+"/Nextcloud/wallhaven-384721.png";
+            String viewer = "eog ";
+        }
 
         //kill all running processes of PhotoViewer
         try{
