b854342016116bc563bc6e39344b442c6a86933a
Yannik Eckert <yannik.eckert@uni-oldenburg.de>
2019-08-09 00:14:43 +0200

    Replaced Strings with ArrayList and String-Array

--- src/dice.java
+++ src/dice.java
@@ -4,63 +4,57 @@ import java.io.*;
 import java.util.*;
 
 public class dice {
-    public static void main(String[] args) {
-        Random rand = new Random();
-        int random = rand.nextInt(3) + 1;
-        String abs_img1 = "";
-        String abs_img2 = "";
-        String abs_img3 = "";
-        String expr = "";
-        String home = System.getProperty("user.home");
-        String img1 = "63740442_p0.png";
-        String img2 = "c7906b68f44be6e1aac7357dd54e520b.jpg";
-        String img3 = "wallhaven-384721.png";
-        String kill_cmd = "";
-        String os = System.getProperty("os.name");
-        String viewer = "";
+	public static void main(String[] args) {
+		Random rand = new Random();
+		int random = rand.nextInt(3);
+		ArrayList<String> absImg = new ArrayList<>();
+		String expr = "";
+		String home = System.getProperty("user.home");
+		String[] imgStrings = { "Z55fNk7.jpg", "c7906b68f44be6e1aac7357dd54e520b.jpg", "wallhaven-384721.png" };
+		String kill_cmd = "";
+		String os = System.getProperty("os.name");
+		String viewer = "";
 
-        // determine which os is being used
-        if (os.equals("Windows 10")) {
-            kill_cmd = "cmd.exe /c taskkill -im rundll32.exe";
-            abs_img1 = home + "\\Nextcloud\\Images\\" + img1;
-            abs_img2 = home + "\\Nextcloud\\Images\\" + img2;
-            abs_img3 = home + "\\Nextcloud\\Images\\" + img3;
-            viewer = "rundll32 \"C:\\Program Files (x86)\\Windows Photo Viewer\\PhotoViewer.dll\", ImageView_Fullscreen ";
-        } else if (os.equals("Linux")) {
-            kill_cmd = "pkill eog";
-            abs_img1 = home + "/Nextcloud/Images/" + img1;
-            abs_img2 = home + "/Nextcloud/Images/" + img2;
-            abs_img3 = home + "/Nextcloud/Images/" + img3;
-            viewer = "eog ";
-        } else {
-            System.out.println("Currently not supported");
-        }
+		// determine which os is being used
+		if (os.equals("Windows 10")) {
+			kill_cmd = "cmd.exe /c taskkill -im rundll32.exe";
+			for (String s : imgStrings)
+				absImg.add(home + "\\Nextcloud\\Images\\" + s);
+			viewer = "rundll32 \"C:\\Program Files (x86)\\Windows Photo Viewer\\PhotoViewer.dll\", ImageView_Fullscreen ";
+		} else if (os.equals("Linux")) {
+			kill_cmd = "pkill eog";
+			for (String s : imgStrings)
+				absImg.add(home + "/Nextcloud/Images/" + s);
+			viewer = "eog ";
+		} else {
+			System.err.println("Currently not supported");
+		}
 
-        try {
-            // kill all running processes of PhotoViewer
-            Runtime.getRuntime().exec(kill_cmd);
+		try {
+			// kill all running processes of PhotoViewer
+			Runtime.getRuntime().exec(kill_cmd);
 
-            // execution of the PhotoViewer with chosen image
-            switch (random) {
-            case 1: {
-                expr = viewer + abs_img1;
-                break;
-            }
-            case 2: {
-                expr = viewer + abs_img2;
-                break;
-            }
-            case 3: {
-                expr = viewer + abs_img3;
-                break;
-            }
-            default:
-                System.out.println("This could be your ad");
-            }
+			// execution of the PhotoViewer with chosen image
+			switch (random) {
+			case 0: {
+				expr = viewer + absImg.get(0);
+				break;
+			}
+			case 1: {
+				expr = viewer + absImg.get(1);
+				break;
+			}
+			case 2: {
+				expr = viewer + absImg.get(2);
+				break;
+			}
+			default:
+				System.out.println("This could be your ad");
+			}
 
-            Runtime.getRuntime().exec(expr);
-        } catch (IOException e) {
-            System.out.println(e);
-        }
-    }
+			Runtime.getRuntime().exec(expr);
+		} catch (IOException e) {
+			System.out.println(e);
+		}
+	}
 }
